6f5d8a55f5c5e509d8bd6a2778eddb198411fae8
Codexign <codexignllc@gmail.com>
2025-08-21 22:09:19 +0000

    bhai profile username image folloowers wagera put karne k badd find pa c

--- src/app/hack/[platform]/[username]/page.tsx
+++ src/app/hack/[platform]/[username]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from 'react';
-import { useParams, useRouter } from 'next/navigation';
+import { useParams, useRouter, useSearchParams } from 'next/navigation';
 import Image from 'next/image';
 import { Card, CardContent, CardFooter, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
